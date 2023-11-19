90f6230d5a88ae5884f2860496086276001c465d
lao-jiawei <lao-jiawei@qq.com>
2023-11-19 20:41:17 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/*
+* @Description: 代码规范
+* @Author: lao-jiawei
+* @Date: 2023-11-19
+* @LastEditTime: 2023-11-19
+* @LastEditors: Please set LastEditors
+*/
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+};
